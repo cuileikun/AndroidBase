7c553a36ba47fee116a91e14921e23b5b561d2a6
cuileikun <583271702@qq.com>
2017-07-01 16:06:22 +0800

    测试能否提交

--- app/src/main/java/com/cuileikun/androidbase/javaactivity/Java24Activity.java
+++ app/src/main/java/com/cuileikun/androidbase/javaactivity/Java24Activity.java
@@ -9,5 +9,6 @@ public class Java24Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_java24);
+        //ceshi
     }
 }
