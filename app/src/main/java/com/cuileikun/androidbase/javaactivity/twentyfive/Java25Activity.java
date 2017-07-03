0616b0cecaf3a7ff7556a0044c59f83c59477326
cuileikun <583271702@qq.com>
2017-07-03 14:23:14 +0800

    day25

--- app/src/main/java/com/cuileikun/androidbase/javaactivity/twentyfive/Java25Activity.java
+++ app/src/main/java/com/cuileikun/androidbase/javaactivity/twentyfive/Java25Activity.java
@@ -1,4 +1,4 @@
-package com.cuileikun.androidbase.javaactivity;
+package com.cuileikun.androidbase.javaactivity.twentyfive;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
